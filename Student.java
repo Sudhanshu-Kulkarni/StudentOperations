13fd9e07a889978068b4d591ceb40a8898497f0e
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-03-03 21:45:52 +0530

    Added constructor to initialise Student class attributes

--- Student.java
+++ Student.java
@@ -2,7 +2,10 @@ public class Student {
     private String name;
     private long prn;
 
-    
+    public Student(String name, long prn) {
+        setName(name);
+        setPRN(prn);
+    }
 }
 
 
